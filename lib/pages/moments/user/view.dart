28e0464e46eb5730923f0088152e9e74811c55fa
Steve Cheng <biao.cheng@thoughtworks.com>
2023-05-22 11:11:50 +0800

    implement the layout of user view

--- lib/pages/moments/user/view.dart
+++ lib/pages/moments/user/view.dart
@@ -1,5 +1,7 @@
 import 'package:first_demo/pages/moments/user/controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_constraintlayout/flutter_constraintlayout.dart';
+import 'package:get/get.dart';
 
 class UserView extends StatelessWidget {
   final UserController _controller;
@@ -10,7 +12,45 @@ class UserView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _controller.data;
-    return Container();
+    final user = _controller.data;
+    final theme = Get.theme;
+
+    return ConstraintLayout(children: [
+      Container(
+        color: theme.colorScheme.secondary,
+        child: Image.network(
+          user.profileImage ?? '',
+          fit: BoxFit.cover,
+        ),
+      ).applyConstraint(
+        width: matchParent,
+        height: 200,
+        top: parent.top,
+      ),
+      Card(
+        color: theme.colorScheme.primary,
+        child: Image.network(
+          width: 80,
+          height: 80,
+          user.avatar ?? '',
+          fit: BoxFit.cover,
+        ),
+      ).applyConstraint(
+        id: ConstraintId('card'),
+        top: parent.top,
+        right: parent.right,
+        margin: const EdgeInsets.only(top: 150, right: 20),
+      ),
+      Text(
+        user.nick ?? user.username ?? '',
+        style: theme.textTheme.titleLarge?.copyWith(
+          color: theme.colorScheme.surface,
+        ),
+      ).applyConstraint(
+        right: ConstraintId('card').left,
+        bottom: ConstraintId('card').bottom,
+        margin: const EdgeInsets.only(right: 10, bottom: 40),
+      ),
+    ]);
   }
 }
