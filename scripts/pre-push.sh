b33902e483406af3ce22f67ef5def2aa7a102649
Biao Cheng <thoughtworks.t.cheng@mercedes-benz.com>
2026-05-28 20:46:51 +0800

    update pre-push script to use project directory for coverage check

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
 
 # Unit tests + Coverage check
-bash "$SCRIPT_DIR/check-coverage.sh"
+bash "$PROJECT_DIR/scripts/check-coverage.sh"
 if [ $? -ne 0 ]; then
   exit 1
 fi
