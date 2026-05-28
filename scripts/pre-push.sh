15495de9d76f300112dc0900cb72c0aa326400c2
Biao Cheng <thoughtworks.t.cheng@mercedes-benz.com>
2026-05-28 16:35:47 +0800

    add coverage check script and integrate it into pre-push hook

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-# Unit tests
-printf "\e[33;1m%s\e[0m\n" '=== Running Unit Tests ==='
-flutter test
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Unit tests + Coverage check
+bash "$SCRIPT_DIR/check-coverage.sh"
 if [ $? -ne 0 ]; then
-  printf "\e[31;1m%s\e[0m\n" '=== Unit tests error ==='
   exit 1
 fi
-printf "\e[33;1m%s\e[0m\n" 'Finished running Unit Tests'
-printf '%s\n' ""
+
